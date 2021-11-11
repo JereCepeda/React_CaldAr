e14fc6a93008d464addff81de02a72203fe69608
lucasboasso <lucassebastian.boasso@alumnos.uai.edu.ar>
2021-11-11 17:56:51 -0300

    Instalacion de redux y su config inicial

--- reactcaldar/src/index.js
+++ reactcaldar/src/index.js
@@ -2,13 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { composeWithDevTools } from 'redux-devtools-extension'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux';
+import { rootReducer } from './redux/reducers/rootReducer'
 import Routes from './routes'
+
+const configureStore = () => {
+  const enhancer = composeWithDevTools();
+  return createStore(rootReducer, enhancer)
+};
+
+const store = configureStore();
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <Routes />
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Routes />
+      </Router>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
